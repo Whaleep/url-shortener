f194095751561194fb78bebb03cd616de85f9a9c
Whaleep <billy_1817@yahoo.com.tw>
2021-07-21 13:34:44 +0800

    style: add copied prompt

--- public/javascript/success.js
+++ public/javascript/success.js
@@ -6,4 +6,9 @@ shortUrl.innerText = document.URL + shortUrl.innerText
 // copy to clipboard. ref: https://iter01.com/582157.html
 copyUrl.addEventListener('click', async () => {
   await navigator.clipboard.writeText(shortUrl.innerText)
+  copyUrl.innerText = 'Copied'
+})
+
+copyUrl.addEventListener('mouseleave',()=>{
+  copyUrl.innerText = 'Copy'
 })
